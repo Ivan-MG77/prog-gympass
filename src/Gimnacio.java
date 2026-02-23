127e1b078ba6cf982b18edbc7c8f7b54dc46a878
ivanm <ivan.m.g.1791@gmail.com>
2026-02-23 22:01:58 +0100

    Clase Gimunasio

---
+++ src/Gimnacio.java
@@ -0,0 +1,13 @@
+public class Gimnacio {
+
+    private String codigo;
+    private String nombre;
+
+
+    public Gimnacio(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+
+    }
+
+}
