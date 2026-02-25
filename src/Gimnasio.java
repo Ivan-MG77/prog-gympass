302210ce3e2cde0677b5b2b4a519fdeffc64a16e
ivanm <ivan.m.g.1791@gmail.com>
2026-02-25 10:19:21 +0100

    feat: metodo existeSocio

--- src/Gimnasio.java
+++ src/Gimnasio.java
@@ -3,8 +3,13 @@ public class Gimnasio {
     //Atributos normalmente en PRIVATE
     private String codigo;
     private String nombre;
-    private Socio socio[];
-    private Sala salas[];
+    private Socio[] listaSocios;
+    private Sala[] listaSalas;
+    private int responsableId;
+
+    public final int MAX_Socios;
+    public final int MAX_Salas;
+
 
     /**
      * Esta es el METODO CONSTRUCTOR de Sala
@@ -13,10 +18,63 @@ public class Gimnasio {
      * @param codigo
      * @param nombre
      */
-    public Gimnasio(String codigo, String nombre) {
+    public Gimnasio(String codigo, String nombre, int cantidadMaxSocios, int cantidadMaxSalas) {
         this.codigo = codigo;
         this.nombre = nombre;
+        this.MAX_Socios = cantidadMaxSocios;
+        this.MAX_Salas = cantidadMaxSalas;
+
+        this.listaSocios = new Socio[MAX_Socios];
+        this.listaSalas = new Sala[MAX_Salas];
+
+        this.responsableId = -1;
+    }
+
+
+    //GESTION DE SOCIOS
+
+    public boolean registrarSocio(Socio socio) {
+        boolean resultadoRegistrado = false;
+
+        if(existeSocio() !=   ) {
+
+
+        }
+        return resultadoRegistrado;
+    }
+
+    public Socio expulsarSocio(int numeroSocio) {
 
     }
+
+    public boolean existeSocio() {
+        boolean resultadoExiteSocio = false;
+
+        for (int i = 0; i < MAX_Socios && !resultadoExiteSocio; i++ ) {
+            if (this.listaSocios[i] != null){
+                resultadoExiteSocio = true;
+            }
+        }
+        return resultadoExiteSocio;
+    }
+
+    public boolean designarResponsable(int numeroSocio){
+
+    }
+
+
+    //GESTION DE SALAS
+
+    public boolean incorporarSala(Sala sala){
+
+    }
+
+    //RESUMEN
+    public String obtenerInforme(){
+
+    }
+
+
+
 
 }
